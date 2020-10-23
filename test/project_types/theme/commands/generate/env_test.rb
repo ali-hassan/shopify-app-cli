41564863f0752992843ff42b8f454f872adbf3f1
Carmela Leung <carmela.leung@shopify.com>
2020-10-23 18:29:23 -0400

    Add tests + Generate, env, and pull

---
+++ test/project_types/theme/commands/generate/env_test.rb
@@ -0,0 +1,189 @@
+# frozen_string_literal: true
+require 'project_types/theme/test_helper'
+
+module Theme
+  module Commands
+    module GenerateTests
+      class EnvTest < MiniTest::Test
+        resp = [200,
+                { "themes" =>
+                 [{ "id" => 2468,
+                    "name" => "my_theme" },
+                  { "id" => 1357,
+                    "name" => "your_theme" }] }]
+        THEMES = resp[1]['themes'].map { |theme| [theme['name'], theme['id']] }.to_h
+
+        CONFIG = { "development" =>
+                   { "password" => "boop",
+                     "theme_id" => "2468",
+                     "store" => "shop.myshopify.com" } }
+
+        def test_prompts_for_credentials
+          FakeFS do
+            File.write('config.yml', CONFIG.to_yaml)
+            context = ShopifyCli::Context.new
+
+            CLI::UI::Prompt.expects(:ask)
+              .with(context.message('theme.generate.env.ask_store_default', 'shop.myshopify.com'))
+              .returns('shop.myshopify.com')
+
+            CLI::UI::Prompt.expects(:ask)
+              .with(context.message('theme.generate.env.ask_password_default', 'boop'))
+              .returns('boop')
+
+            Themekit.expects(:query_themes)
+              .with(context, store: 'shop.myshopify.com', password: 'boop')
+              .returns(THEMES)
+            CLI::UI::Prompt.expects(:ask)
+              .with(context.message('theme.generate.env.ask_theme'))
+              .returns('2468')
+
+            Themekit.expects(:generate_env)
+              .with(context,
+                    password: 'boop',
+                    themeid: '2468',
+                    store: 'shop.myshopify.com',
+                    env: nil)
+              .returns(true)
+
+            command = Theme::Commands::Generate::Env.new(context)
+            command.call
+          end
+        end
+
+        def test_can_use_default_shop_and_password
+          FakeFS do
+            File.write('config.yml', CONFIG.to_yaml)
+            context = ShopifyCli::Context.new
+
+            CLI::UI::Prompt.expects(:ask)
+              .with(context.message('theme.generate.env.ask_store_default', 'shop.myshopify.com'))
+              .returns('')
+
+            CLI::UI::Prompt.expects(:ask)
+              .with(context.message('theme.generate.env.ask_password_default', 'boop'))
+              .returns('')
+
+            Themekit.expects(:query_themes)
+              .with(context, store: 'shop.myshopify.com', password: 'boop')
+              .returns(THEMES)
+            CLI::UI::Prompt.expects(:ask)
+              .with(context.message('theme.generate.env.ask_theme'))
+              .returns('2468')
+
+            Themekit.expects(:generate_env)
+              .with(context,
+                    password: 'boop',
+                    themeid: '2468',
+                    store: 'shop.myshopify.com',
+                    env: nil)
+              .returns(true)
+
+            command = Theme::Commands::Generate::Env.new(context)
+            command.call
+          end
+        end
+
+        def test_can_provide_credentials_with_flags
+          FakeFS do
+            File.write('config.yml', CONFIG.to_yaml)
+            context = ShopifyCli::Context.new
+
+            CLI::UI::Prompt.expects(:ask)
+              .with(context.message('theme.generate.env.ask_store_default', 'shop.myshopify.com'))
+              .never
+
+            CLI::UI::Prompt.expects(:ask)
+              .with(context.message('theme.generate.env.ask_password_default', 'boop'))
+              .never
+
+            Themekit.expects(:query_themes)
+              .with(context, store: 'shop.myshopify.com', password: 'boop')
+              .never
+            CLI::UI::Prompt.expects(:ask)
+              .with(context.message('theme.generate.env.ask_theme'))
+              .never
+
+            Themekit.expects(:generate_env)
+              .with(context,
+                    password: 'boop',
+                    themeid: '2468',
+                    store: 'shop.myshopify.com',
+                    env: nil)
+              .returns(true)
+
+            command = Theme::Commands::Generate::Env.new(context)
+            command.options.flags[:store] = 'shop.myshopify.com'
+            command.options.flags[:password] = 'boop'
+            command.options.flags[:themeid] = '2468'
+            command.call
+          end
+        end
+
+        def test_can_use_different_env
+          FakeFS do
+            File.write('config.yml', CONFIG.to_yaml)
+            context = ShopifyCli::Context.new
+
+            CLI::UI::Prompt.expects(:ask)
+              .with(context.message('theme.generate.env.ask_store_default', 'shop.myshopify.com'))
+              .returns('')
+
+            CLI::UI::Prompt.expects(:ask)
+              .with(context.message('theme.generate.env.ask_password_default', 'boop'))
+              .returns('')
+
+            Themekit.expects(:query_themes)
+              .with(context, store: 'shop.myshopify.com', password: 'boop')
+              .returns(THEMES)
+            CLI::UI::Prompt.expects(:ask)
+              .with(context.message('theme.generate.env.ask_theme'))
+              .returns('2468')
+
+            Themekit.expects(:generate_env)
+              .with(context,
+                    password: 'boop',
+                    themeid: '2468',
+                    store: 'shop.myshopify.com',
+                    env: 'test')
+              .returns(true)
+
+            command = Theme::Commands::Generate::Env.new(context)
+            command.options.flags[:env] = 'test'
+            command.call
+          end
+        end
+
+        def test_not_allow_empty_if_no_config
+          context = ShopifyCli::Context.new
+
+          CLI::UI::Prompt.expects(:ask)
+            .with(context.message('theme.generate.env.ask_store'), allow_empty: false)
+            .returns('shop.myshopify.com')
+
+          CLI::UI::Prompt.expects(:ask)
+            .with(context.message('theme.generate.env.ask_password'), allow_empty: false)
+            .returns('boop')
+
+          Themekit.expects(:query_themes)
+            .with(context, store: 'shop.myshopify.com', password: 'boop')
+            .returns(THEMES)
+          CLI::UI::Prompt.expects(:ask)
+            .with(context.message('theme.generate.env.ask_theme'))
+            .returns('2468')
+
+          Themekit.expects(:generate_env)
+            .with(context,
+                  password: 'boop',
+                  themeid: '2468',
+                  store: 'shop.myshopify.com',
+                  env: nil)
+            .returns(true)
+
+          command = Theme::Commands::Generate::Env.new(context)
+          command.call
+        end
+      end
+    end
+  end
+end
